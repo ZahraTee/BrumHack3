2288cbf7b8cfb25af8aa9bcc17266738e9bd8d2e
zahra <zahratee@gmail.com>
2015-10-24 20:38:05 +0100

    Adding img alt tags with clarifai keywords

--- ChromeExt/scrape.js
+++ ChromeExt/scrape.js
@@ -1,30 +1,26 @@
 var i, imgs, token;
 getToken();
 
-
 function getToken()
 {
 	$.post("https://api.clarifai.com/v1/token/", {grant_type: "client_credentials", client_id: "0tibu3hLLX0yM_B1IZj_wfIyweqsjhn6w5XRS41O", client_secret: "uS8pDSrBUiR3ZucHLcIrDcU0MMYObopodjU0zmkD"}, function(result){
         console.log(result);
         token = result;
-
         imgs = $('img');
-	for (i = 0; i < imgs.length; i++) {
-	// console.log(imgs[i].src);
-	getImageTagsFromURL(imgs[i].src);
-}
-    });
+		for (i = 0; i < imgs.length; i++) {
+			var alttext = getImageTagsFromURL(imgs[i], imgs[i].src);
+		}
+	});
 }
 
-function getImageTagsFromURL(image_url)
+function getImageTagsFromURL(image_element,image_url)
 {
 	if (image_url == null || image_url == "") { return; }
 	var pic_formats = ["jpg", "jpeg", "png", "gif", "bmp", "tiff"];
 	var count;
-	for (var i in pic_formats) {
-		if (endsWith(image_url, pic_formats[i])) count++;
-	}
+	for (var i in pic_formats) { if (endsWith(image_url, pic_formats[i])) count++; }
 	if (count == 0) return;
+
 	$.ajax({
 		url: 'https://api.clarifai.com/v1/tag/',
 		type: 'POST',
@@ -32,10 +28,21 @@ function getImageTagsFromURL(image_url)
 		    xhr.setRequestHeader('Authorization', 'bearer ' + "3g69QuTW94KoJnWHEkzNmqtvmKMYOQ");
 		},
 		data: {url: image_url},
-		success: function (object) { console.log(object.results[0].result.tag.classes[0])},
-		error: function (err) { console.log("Couldn't get image tags! " + image_url)},
+		success: function (object) {
+			for (i = 0; i < object.results.length; i++) { 
+				
+					var keywords = object.results[i].result.tag.classes;
+					var alttext = ""
+					for (j = 0; j < keywords.length; j++)
+					{
+						alttext = alttext + " " + keywords[j];
+					}
+					$(imgs[i]).attr("alt", "Clarifai Image:" + alttext);
+					console.log($(imgs[i]).attr("alt"));
+				}
+			},
+		error: function (err) { return "Unreadable image."},
 		});
-
 }
 
 function endsWith(str, suffix) {
