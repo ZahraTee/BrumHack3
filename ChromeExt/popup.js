d0806c203f141dba052dd7cf9b85b715a2c28098
Carolyn Yao <cyao42@gmail.com>
2015-10-25 10:13:15 +0000

    added input field

--- ChromeExt/popup.js
+++ ChromeExt/popup.js
@@ -116,3 +116,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
